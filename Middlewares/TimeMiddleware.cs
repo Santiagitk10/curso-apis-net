0524ab82f7230d2102e6717ccb46fe354726d069
Santiagitk10 <93751091+Santiagitk10@users.noreply.github.com>
2023-04-03 12:40:25 -0500

    Update TimeMiddleware.cs

--- Middlewares/TimeMiddleware.cs
+++ Middlewares/TimeMiddleware.cs
@@ -11,11 +11,14 @@ public class TimeMiddleware
     //HttpContext representa el requerimiento que viene en la petición http
     public async Task Invoke(Microsoft.AspNetCore.Http.HttpContext context)
     {
+        //Si en los parámetros del requerimiento tenga la key "time"
         if(context.Request.Query.Any(p=> p.Key == "time"))
         {
+            //En la respuesta del context se escribe la hora del servidor
             await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
         }
 
+        //Una vez se haya completado el middleware se llama al siguiente middleware
         await next(context);
     }
 
@@ -24,6 +27,8 @@ public class TimeMiddleware
 
     public static class TimeMiddlewareExtension
     {
+        //Se recibe el IApplicationBuilder y se retorna con el middleware creado ya aplicado, por eso se usa this
+        //en los parámetros. 
         public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
         {
             return builder.UseMiddleware<TimeMiddleware>();
