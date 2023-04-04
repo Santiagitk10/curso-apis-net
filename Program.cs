9adc9f5e51f4896b650bd2ea3e3f802e499d4e9c
Santiagitk10 <93751091+Santiagitk10@users.noreply.github.com>
2023-04-04 11:03:40 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//EL CONNECTION STRING POR BUENA PRÁCTICA SE DEBE ES AGREGAR EN EL APPSETTINGS Y LLAMARLO CON ARCHIVO DE CONFIGURACIÓN Y SECRETOS, ACÁ SE HACE DIRECTAMENTE EN EL PROGRAM
 builder.Services.AddSqlServer<TareasContext>("Data Source=server;Initial Catalog=TareasDb;user id=sa;password=pass");
 //builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
 builder.Services.AddScoped<IHelloWorldService>(p=> new HelloWorldService());
